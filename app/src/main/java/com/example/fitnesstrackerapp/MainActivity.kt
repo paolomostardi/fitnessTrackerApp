f8b8465a6af0891bef32974e774d442ec432741e
Paolo Mostardi <paolomostardi@gmail.com>
2022-05-10 13:28:07 +0100

    update

---
+++ app/src/main/java/com/example/fitnesstrackerapp/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.example.fitnesstrackerapp
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import android.os.Build
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    @SuppressLint("MissingPermission")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        println("---------------- App starts ----------------")
+        // todo make this available for any build
+        val locationPermissionRequest = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+
+        ) { permissions ->
+
+                when {
+                    permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
+                        // Precise location access granted.
+                        println("ACCESS_FINE_LOCATION granted     ${fusedLocationClient.lastLocation}")
+
+                    }
+                    permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
+                        // Only approximate location access granted.
+                        val someStuff = fusedLocationClient.getLastLocation().result
+                        println("ACCESS_COARSE_LOCATION granted ${someStuff}")
+                    } else -> {
+                    // No location access granted.
+                        println("no access granted")
+                }
+                }
+
+        }
+        locationPermissionRequest.launch(arrayOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION))
+    }
+}
